5edc7b6f0119ee908e731e0b11b4cd681b5633ed
n3350 <Jiuso1@jesusgg200x@gmail.com>
2024-06-16 16:59:26 +0200

    Trying to optimize file reading.

--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 #include <string.h>
 #include <time.h>
 
+#define SIZE 512
+
 int main(void)
 {
     char *buffer = (char *)malloc(sizeof(char)); // Dynamic buffer used to read input from file. At first, it just points to one char.
     size_t bufferSize = 1;                       // Only one char.
     FILE *inputFile = fopen("input.txt", "r");   // Pointer to input file, using reading mode and text mode.
-    char inputFileCharacter = '\0';              // Char that will save each character readed from file.
     clock_t start = 0;                           // Start time.
     clock_t end = 0;                             // Finish time.
     double timeSpent = 0;                        // Time that took whatever we measure.
+    char inputFileString[SIZE] = "a";            // Array to save each codon from file.
 
-    strncpy(buffer, "", bufferSize); //"", the \0 character.
+    strncpy(buffer, "", bufferSize); //"", which converts to the \0 character.
 
     if (inputFile == NULL) // If the input file couldn't be opened:
     {
@@ -23,19 +25,21 @@ int main(void)
     }
     else
     {
-        start = clock(); // We get start time.
         // If the input file could be opened:
-        //  Character reading based on https://www.codevscolor.com/c-program-read-file-contents-character
-        while (inputFileCharacter != EOF) // While the file hasn't finished:
+        start = clock(); // We get start time.
+        //  Three-character reading based on https://www.geeksforgeeks.org/fread-function-in-c/
+        while (!feof(inputFile) && (strncmp(inputFileString, "", SIZE) != 0)) // While the file hasn't finished:
         {
-            inputFileCharacter = fgetc(inputFile); // We get a char from file.
-            if (inputFileCharacter != EOF)         // If the file hasn't finished:
+            fread(inputFileString, sizeof(inputFileString), 1, inputFile);
+            printf("%s\n", inputFileString);
+            if ((strncmp(inputFileString, "", SIZE) != 0)) // If the file hasn't finished:
             {
-                bufferSize++;                                 // The buffer size increases to save the new char in buffer.
-                buffer = (char *)realloc(buffer, bufferSize); // The buffer reallocates to supply the new buffer size. Previous string stay in buffer.
-                if (buffer != NULL)                           // If the reallocation was good:
+                printf("%s\n", inputFileString);
+                bufferSize += SIZE;                                          // The buffer size increases to save the new codon in buffer.
+                buffer = (char *)realloc(buffer, bufferSize * sizeof(char)); // The buffer reallocates to supply the new buffer size. Previous string stay in buffer.
+                if (buffer != NULL)                                          // If the reallocation was good:
                 {
-                    strncat(buffer, &inputFileCharacter, 1); // The new char is appended to the previous string of buffer.
+                    strncat(buffer, inputFileString, sizeof(inputFileString)); // The new codon is appended to the previous string of buffer.
                 }
             }
         }
@@ -48,12 +52,12 @@ int main(void)
     char output[size];                // Output string processed.
 
     strncpy(input, buffer, bufferSize); // Buffer is copied to input.
-    free(buffer);                       // We free buffer memory, as contents were copied to input.
+
+    free(buffer); // We free buffer memory, as contents were copied to input.
 
     end = clock(); // We get end time.
 
-    timeSpent = (double)(end - start) / CLOCKS_PER_SEC; // We get seconds.
-    timeSpent *= 1000;                                  // We transform seconds to milliseconds.
+    timeSpent = ((double)(end - start) / CLOCKS_PER_SEC) * 1000; // We get seconds and we transform to milliseconds.
 
     printf("Reading from input.txt lasted %f milliseconds\n", timeSpent);
 
@@ -61,8 +65,7 @@ int main(void)
     replaceInput(input, output, size); // Input solution processed in output string.
     end = clock();                     // We get end time.
 
-    timeSpent = (double)(end - start) / CLOCKS_PER_SEC; // We get seconds.
-    timeSpent *= 1000;                                  // We transform seconds to milliseconds.
+    timeSpent = ((double)(end - start) / CLOCKS_PER_SEC) * 1000; // We get seconds and we transform to milliseconds.
 
     // printf("%s\n", output);
 
